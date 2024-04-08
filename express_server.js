b08d6c03fc23c5d226f6eb5c7ff654add23b79c3
Brandt <bseibs>
2024-04-08 13:03:24 -0700

    blank host page currently

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
-app.set("view engine", "ejs");
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -16,8 +17,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
